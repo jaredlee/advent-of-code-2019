b50c8c36d9705737e23bd623942ab82138ec3783
jared <jaredl607@gmail.com>
2019-12-15 16:45:51 -0700

    preparing day 2

---
+++ src/day02/mod.rs
@@ -0,0 +1,34 @@
+use std::cmp;
+use std::fs;
+
+pub fn solve_part_one(input_file: &str) -> i64
+{
+    let contents = fs::read_to_string(input_file)
+        .expect(&format!("Unable to read file {}", input_file));
+
+    0
+}
+
+pub fn solve_part_two(input_file: &str) -> i64
+{
+    let contents = fs::read_to_string(input_file)
+        .expect(&format!("Unable to read file {}", input_file));
+
+    0
+}
+
+#[cfg(test)]
+mod examples
+{
+    use crate::day02::*;
+
+    #[test]
+    fn part_one()
+    {
+    }
+
+    #[test]
+    fn part_two()
+    {
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,13 @@
 mod day01;
+mod day02;
 
 fn main() 
 {
     println!("Day 01 Part 1: {}", day01::solve_part_one("src/day01/input.txt"));
     println!("Day 01 Part 2: {}", day01::solve_part_two("src/day01/input.txt"));
+
+    println!("Day 02 Part 1: {}", day02::solve_part_one("src/day02/input.txt"));
+    println!("Day 02 Part 2: {}", day02::solve_part_two("src/day02/input.txt"));
 }
 
 #[cfg(test)]
@@ -16,5 +20,12 @@ mod answers
         use crate::day01::*;
         assert_eq!(solve_part_one("src/day01/input.txt"), 3427972);
         assert_eq!(solve_part_two("src/day01/input.txt"), 5139078);
+    }
+
+    fn day02()
+    {
+        use crate::day02::*;
+        // assert_eq!(solve_part_one("src/day01/input.txt"), 3427972);
+        // assert_eq!(solve_part_two("src/day01/input.txt"), 5139078);
     }
 }
